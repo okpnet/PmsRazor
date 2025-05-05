d7802bb05a34a6d527a4c269780152b81686faa6
hayato <umai-beer@hotmail.co.jp>
2025-05-05 17:11:02 +0900

    ok

--- QualRazorCore/QualRazorCore/Controls/Conditions/FilterField.razor.cs
+++ QualRazorCore/QualRazorCore/Controls/Conditions/FilterField.razor.cs
@@ -1,4 +1,6 @@
-﻿using QualRazorCore.Core;
+﻿using Microsoft.AspNetCore.Components;
+using QualRazorCore.Controls.Conditions.Options;
+using QualRazorCore.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,7 @@ namespace QualRazorCore.Controls.Conditions
 {
     public partial class FilterField:RazorCore
     {
+        [Parameter]
+        public FilterOption Option { get; set; } = new();
     }
 }
---
+++ QualRazorCore/QualRazorCore/Controls/Conditions/Options/FilterOption.cs
@@ -0,0 +1,29 @@
+﻿using QualRazorCore.Controls.Conditions.Core;
+using QualRazorCore.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualRazorCore.Controls.Conditions.Options
+{
+    public class FilterOption:NotifyCore
+    {
+        PropertyCondition _propertyConditions=default!;
+
+        public PropertyCondition PropertyConditions
+        {
+            get => _propertyConditions;
+            set
+            {
+                if (Equals(_propertyConditions , value))
+                {
+                    return; 
+                }
+                _propertyConditions = value;
+                OnPropertyChanged(nameof(PropertyConditions));
+            }
+        }
+    }
+}
