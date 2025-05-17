038a9555cdd8b04661059607d198307d16f7a413
umai-beer <umai-beer@hotmail.co.jp>
2025-05-17 22:20:57 +0900

    ok

---
+++ QualRazorCore/QualRazorCore/Controls/Tables/Parameters/ColumnParameter.cs
@@ -0,0 +1,15 @@
+﻿using QualRazorCore.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualRazorCore.Controls.Tables.Parameters
+{
+    public class ColumnParameter<TModel, TResult> : PropertyAccessCore<TModel, TResult>, INotifyPropertyChanged
+    {
+
+    }
+}
--- QualRazorCore/QualRazorCore/Controls/Tables/TableContent.razor.cs
+++ QualRazorCore/QualRazorCore/Controls/Tables/TableContent.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
 using QualRazorCore.Controls.Tables.Columns.Core;
 using QualRazorCore.Controls.Tables.Extensions;
 using QualRazorCore.Controls.Tables.Helpers;
@@ -8,6 +9,8 @@ using QualRazorCore.Controls.Tables.Rows.Core;
 using QualRazorCore.Core;
 using QualRazorCore.Extenssions;
 using QualRazorCore.Observers;
+using System.Collections.ObjectModel;
+using System.Linq.Expressions;
 
 namespace QualRazorCore.Controls.Tables
 {
@@ -21,7 +24,7 @@ namespace QualRazorCore.Controls.Tables
         [Parameter,EditorRequired]
         public TableSchemaOption<TModel> Option { get; set; } = default!;
 
-        IEnumerable<ITableColumn<TModel>> Columns=>Option.Columns.OfType<ITableColumn<TModel>>();
+        IEnumerable<PropertyAccessCore> Columns { get; set; }=new ObservableCollection<PropertyAccessCore>();
 
         protected int NumberOfColumn => Columns.Count();
 
@@ -50,9 +53,40 @@ namespace QualRazorCore.Controls.Tables
                 );
         }
 
-        public class ColumnTemplate:RazorCore
+        public class Columns:RazorCore
         {
+            [CascadingParameter(Name = "TableContentParent")]
+            public TableContent<TModel> TableParent { get; set; } = default!;
+            [Parameter]
+            public RenderFragment? ChildContent { get; set; }
 
+            protected override void BuildRenderTree(RenderTreeBuilder builder)
+            {
+                builder.AddContent(0, ChildContent);
+            }
+        }
+
+        public class Column<TPorperty> : RazorCore
+        {
+            [CascadingParameter(Name = "TableContentParent")]
+            public TableContent<TModel> TableParent { get; set; } = default!;
+            [Parameter,EditorRequired]
+            public Expression<Func<TModel, TPorperty>>? Property { get; set; }
+
+            [Parameter]
+            public RenderFragment? HeaderTemplate { get; set; }
+
+            [Parameter]
+            public Func<TModel, RenderFragment>? CellTemplate { get; set; }
+
+            protected override void OnInitialized()
+            {
+                if (TableParent != null)
+                {
+
+                    TableParent.AddColumn(this);
+                }
+            }
         }
     }
 }
