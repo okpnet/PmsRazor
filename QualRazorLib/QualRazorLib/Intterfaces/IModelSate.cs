48b5b04a28fea9b640d1e2d3f75e7d47f4fecf55
hayato <umai-beer@hotmail.co.jp>
2025-05-23 09:33:44 +0900

    ok

---
+++ QualRazorLib/QualRazorLib/Intterfaces/IModelSate.cs
@@ -0,0 +1,16 @@
+﻿using QualRazorLib.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualRazorLib.Intterfaces
+{
+    public interface IModelSate
+    {
+        SortType SortTypes { get; }
+
+
+    }
+}
