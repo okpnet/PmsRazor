f5a21aa804d11c9637665e8d35485a84f444e105
hayato <umai-beer@hotmail.co.jp>
2025-05-28 07:31:23 +0900

    ok

---
+++ QualRazorLib/QualRazorLib/Controls/Tables/Rows/Row.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using QualRazorLib.Controls.Tables.Columns.Dtos;
+using QualRazorLib.Controls.Tables.Rows.Dtos;
+using QualRazorLib.Core;
+using QualRazorLib.Helpers;
+using QualRazorLib.Observer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualRazorLib.Controls.Tables.Rows
+{
+    public partial class Row<TModel> : QualRazorComponentBase
+    {
+        [CascadingParameter(Name = "TableContentParent")]
+        public QualTable<TModel> TableParent { get; set; } = default!;
+
+        [Parameter, EditorRequired]
+        public TableRowState<TModel> Row { get; set; } = default!;
+
+        protected IEnumerable<IColumnState> Columns => TableParent.Columns;
+
+        protected RowStatus RowStatus { get; set; }
+
+        protected Dictionary<string, object> MergedAttributes =>
+        HtmlAttributeHelper.PurgeAttributes(
+            MeargeAttributeBase,
+            new()
+            {
+                [HtmlAtributes.CLASS] = Row.Status.HasFlag(RowStatus.Seleted) ? CssClasses.Table.ROW_SELECTED : "",
+            });
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            RowStatus = Row.Status;
+            PropertyChangedRelay<TableRowState<TModel>, RowStatus>.Create(Row, nameof(Row.Status),
+                t => t.Status,
+                t => RowStatus = t);
+        }
+    }
